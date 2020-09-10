d2bbeddd4063c581d19a358c75558c4265f0fe43
Luis Fraga <luis5944@gmail.com>
2020-09-10 14:13:50 +0200

    f

--- src/api/fetchAPI.js
+++ src/api/fetchAPI.js
@@ -4,10 +4,9 @@ const url = "https://covid19.mathdro.id/api";
 const ulrNews =
   "https://newsapi.org/v2/everything?language=es&q=coronavirus&from=2020-08-10&sortBy=publishedAt&apiKey=8d461b4b16534642b955473385154874";
 export const fetchNews = async () => {
-
-  const response = await fetch(ulrNews);
-  const {articles} = await response.json();
-  // const {data:{articles}} = await axios.get(ulrNews);
+  const {
+    data: { articles },
+  } = await axios.get(ulrNews);
   return articles;
 };
 
