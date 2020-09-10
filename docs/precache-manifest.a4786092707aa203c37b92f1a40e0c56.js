665d0fcd223de575e520014b7564157cbe00f30e
Luis Fraga <luis5944@gmail.com>
2020-09-10 13:20:12 +0200

    docs

---
+++ docs/precache-manifest.a4786092707aa203c37b92f1a40e0c56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f54b5e8c2bd9c8ade077309bebfbbae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "309f818108c734eacdb3",
+    "url": "/static/css/main.8972c70d.chunk.css"
+  },
+  {
+    "revision": "a4ef453d87e68cf5bea8",
+    "url": "/static/js/2.9a72cfb7.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/static/js/2.9a72cfb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "309f818108c734eacdb3",
+    "url": "/static/js/main.c1be1df2.chunk.js"
+  },
+  {
+    "revision": "8862016860622b01e6aa",
+    "url": "/static/js/runtime-main.f22b2772.js"
+  },
+  {
+    "revision": "a402cb446e18407ee5b632ffd9d54aad",
+    "url": "/static/media/covid.a402cb44.png"
+  }
+]);
